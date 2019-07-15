2d61ce67f4164288534a42f8b221a9dd74322138
Maryna Dumenko <m.kushneruk.adamivna@gmail.com>
2019-07-15 21:55:41 +0300

    Updates

---
+++ precache-manifest.2a8825e2db0b3f0ddb0105a2a22d9f98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd32823986a1bfb79c70d8eff07e6ab",
+    "url": "/react-homework/index.html"
+  },
+  {
+    "revision": "b9043bd849e8f02e0c4c",
+    "url": "/react-homework/static/css/main.546977c4.chunk.css"
+  },
+  {
+    "revision": "2d0186e0f6cc9a60b9e8",
+    "url": "/react-homework/static/js/2.ea107c39.chunk.js"
+  },
+  {
+    "revision": "b9043bd849e8f02e0c4c",
+    "url": "/react-homework/static/js/main.fa6516fb.chunk.js"
+  },
+  {
+    "revision": "130ce4bb93aee9d94a56",
+    "url": "/react-homework/static/js/runtime~main.1692f5eb.js"
+  }
+]);
